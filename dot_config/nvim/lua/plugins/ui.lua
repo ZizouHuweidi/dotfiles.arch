8fcc76355253b33713df88a26d50ae1e5c22aced
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2024-02-05 22:56:25 +0100

    zen

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,61 +1,71 @@
 return {
-  "nvimdev/dashboard-nvim",
-  opts = function()
-    local logo = [[
- _____   _                  _    ___         
-/__  /  (_)___  ____  __  _| |  / (_)___ ___ 
+  {
+    "nvimdev/dashboard-nvim",
+    opts = function()
+      local logo = [[
+ _____   _                  _    ___
+/__  /  (_)___  ____  __  _| |  / (_)___ ___
   / /  / /_  / / __ \/ / / / | / / / __ `__ \
  / /__/ / / /_/ /_/ / /_/ /| |/ / / / / / / /
-/____/_/ /___/\____/\__,_/ |___/_/_/ /_/ /_/ 
-                                             
+/____/_/ /___/\____/\__,_/ |___/_/_/ /_/ /_/
+
     ]]
 
-    logo = string.rep("\n", 8) .. logo .. "\n\n"
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
 
-    local opts = {
-      theme = "doom",
-      hide = {
-        -- this is taken care of by lualine
-        -- enabling this messes up the actual laststatus setting after loading a file
-        statusline = false,
-      },
-      config = {
-        header = vim.split(logo, "\n"),
-        -- stylua: ignore
-        center = {
-          { action = "Telescope find_files",              desc = " Find file",       icon = " ", key = "f" },
-          { action = "ene | startinsert",                 desc = " New file",        icon = " ", key = "n" },
-          { action = "Telescope oldfiles",                desc = " Recent files",    icon = " ", key = "r" },
-          { action = "Telescope live_grep",               desc = " Find text",       icon = " ", key = "g" },
-          {action =  "Telescope projects",                desc = " Projects",        icon = " ", key = "p" },
-          { action = "e $MYVIMRC",                        desc = " Config",          icon = " ", key = "c" },
-          { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
-          { action = "Lazy",                              desc = " Lazy",            icon = "󰒲 ", key = "l" },
-          { action = "qa",                                desc = " Quit",            icon = " ", key = "q" },
+      local opts = {
+        theme = "doom",
+        hide = {
+          -- this is taken care of by lualine
+          -- enabling this messes up the actual laststatus setting after loading a file
+          statusline = false,
         },
-        footer = function()
-          local stats = require("lazy").stats()
-          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-        end,
-      },
+        config = {
+          header = vim.split(logo, "\n"),
+          -- stylua: ignore
+          center = {
+            { action = "Telescope find_files", desc = " Find file", icon = " ", key = "f" },
+            { action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
+            { action = "Telescope oldfiles", desc = " Recent files", icon = " ", key = "r" },
+            { action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
+            { action = "Telescope projects", desc = " Projects", icon = " ", key = "p" },
+            { action = "e $MYVIMRC", desc = " Config", icon = " ", key = "c" },
+            { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
+            { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
+            { action = "qa", desc = " Quit", icon = " ", key = "q" },
+          },
+          footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          end,
+        },
+      }
+
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+      end
+
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "DashboardLoaded",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+
+      return opts
+    end,
+  },
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
     }
-
-    for _, button in ipairs(opts.config.center) do
-      button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-    end
-
-    -- close Lazy and re-open when the dashboard is ready
-    if vim.o.filetype == "lazy" then
-      vim.cmd.close()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "DashboardLoaded",
-        callback = function()
-          require("lazy").show()
-        end,
-      })
-    end
-
-    return opts
-  end,
+  },
 }
